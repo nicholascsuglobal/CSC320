74ac22500ce42d40f28ce73a4686d910fe37bcef
nicholas <nicholas.mercier@csuglobal.edu>
2024-02-19 17:42:27 -0700

    add

---
+++ module1.java
@@ -0,0 +1,17 @@
+package module1;
+public class PrintPersonInformation {
+    public static void main(String[] args){
+	    //Declare variables
+		String firstName = "John";
+		String lastName = "Smith";
+		String streetAddress = "555 Cedar Lane";
+		String city = "Los Angeles";
+		String zipCode = "86844";
+		// Print information
+		System.out.println("First Name: " +firstName);
+		System.out.println("Last Name: " +lastName);
+		System.out.println("Street Address: " + streetAddress);
+		System.out.println("City: " + city);
+		System.out.println("Zip Code: " + zipCode);
+    }}
+		
